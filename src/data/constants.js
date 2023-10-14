edec24054884244f0adc63f33de62e7f14677145
Devrast <rastogidev3@gmail.com>
2023-10-14 20:25:10 +0530

    First

---
+++ src/data/constants.js
@@ -0,0 +1,159 @@
+export const Bio = {
+  name: "Dev Rastogi",
+  roles: [
+    "Software Engineer",
+    "Web Developer",
+    "Programmer",
+    "Full Stack Developer",
+  ],
+  description: "I'm a motivated and flexible individual that is always looking for new possibilities to succeed. I am dedicated to delivering top-notch results since I have a strong enthusiasm for learning. I'm ready to have a big effect and do amazing things because I'm optimistic and committed to personal development",
+  github: "https://github.com/Devrast",
+  resume: "https://drive.google.com/file/d/1Zk_SUwt7tRaS-2ZEpDzE-FRdKwj4btDt/view",
+  linkedin: "https://www.linkedin.com/in/rastogidev3/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Java",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg",
+      },
+      {
+        name: "Python",
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+      },
+      {
+        name: "C++",
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg",
+      },
+      {
+        name: "MySQL",
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
+      },
+
+    ],
+  },
+  {
+    title: "Android",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/portfolio-17e0a.appspot.com/o/image_2023-10-12_111529744.png?alt=media&token=ef9e0403-be8e-4210-b7f0-0b0b6146759b&_gl=1*amlnvt*_ga*NTA4NDU5MzA4LjE2OTcwODQwOTE.*_ga_CW55HF8NVT*MTY5NzA4OTQzMy4yLjEuMTY5NzA4OTUzNC40Ny4wLjA.", //not
+    school: "Vellore Instutute of Technology, Chennai",
+    date: "Sept 2021 - July 2025",
+    grade: "8.82 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Vellore Instutute of Technology, Chennai. I have a CGPA of 8.82. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, and Computer Networks, among others.",
+    degree: "Bachelor of Technology - BTech Specializiation in AI and Robotics, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    school: "City Montessori School,Lucknow",
+    date: "Apr 2020 - Apr 2021",
+    grade: "93%",
+    desc: "I completed my Schooling from, City Montessori School, where I studied Science.",
+    degree: "ISC(XII), Science",
+  },
+  {
+    id: 2,
+    school: "City Montessori School,Lucknow",
+    date: "Apr 2018 - Apr 2019",
+    grade: "85%",
+    desc: "I completed my Schooling from, City Montessori School, where I studied Science.",
+    degree: "ICSE(X), Science",
+  },
+];
+
+export const projects = [
+  {
+      id: 0,
+      title: "New York Taxi Trip Duration",
+      date: "June 2023 - July 2023",
+      description:
+        "Implemented a data science project predicting New York taxi trip durations using machine learning. Trained and optimized a model on historical trip data, achieving accurate forecasts. Utilized regression and ensemble algorithms, evaluating performance with mean squared error. Project aimed to optimize routes, estimate fares, and enhance transportation planning",
+      image:
+        "https://github.com/Devrast/New-York-Taxi-Trip/blob/main/download.jpeg?raw=true",
+      tags: [
+        "Python",
+        "Data Science",
+        "Machine Learning"
+      ],
+      category: "Machine Learning",
+      github: "https://github.com/Devrast/New-York-Taxi-Trip",
+  }
+];
